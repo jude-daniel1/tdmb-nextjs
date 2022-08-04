815e2699b50e85db0db41f9c08229bec83fa8302
jude-daniel1 <judedaniel360@gmail.com>
2022-08-04 03:21:28 +0100

    complete header

--- components/HeaderIcon.jsx
+++ components/HeaderIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeaderIcon({ Icon, title }) {
   return (
-    <div>
+    <div className="flex flex-col items-center space-y-2 cursor-pointer hover:text-white active:text-red-400 ">
       <Icon className="h-8" />
       <p>{title}</p>
     </div>
